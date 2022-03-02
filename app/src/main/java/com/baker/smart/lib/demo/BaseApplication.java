199fd036192fbfb1d6bb3db0c15a6a03515d87ee
hushuaijun_github <hushuaijun@data-baker.com>
2022-03-02 17:42:22 +0800

    更新部分授权信息

--- app/src/main/java/com/baker/smart/lib/demo/BaseApplication.java
+++ app/src/main/java/com/baker/smart/lib/demo/BaseApplication.java
@@ -6,10 +6,10 @@ import com.baker.smart.guy.lib.SmartGuyEngine;
 
 public class BaseApplication extends Application {
 
-    public static final String ttsClientId = "d957f3355c7144c8afa2d0e0950b0563";
-    public static final String ttsSecret = "40940788f0734f1b801d71a96d98d76e";
-    public static final String asrClientId = "a34947e4d03240b88ba10e551e24ed38";
-    public static final String asrSecret = "f62071b8845245cea4aa55aaddb76285";
+    public static final String ttsClientId = "your clientId";
+    public static final String ttsSecret = "your secret";
+    public static final String asrClientId = "your clientId";
+    public static final String asrSecret = "your secret";
 
     @Override
     public void onCreate() {
